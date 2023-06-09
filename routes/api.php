43128ee33adf7b0ce33f2d591d8f38ad399386b6
Ahmad Kamal Anwar <ahmad.kamalanwar23@gmail.com>
2023-06-09 20:42:02 +0500

    Make connection with db container

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
